6764af8b8df8f280a3b028fb73b146536100a405
Florian Schmaus <flo@geekplace.eu>
2016-07-31 15:41:04 +0200

    Initial

---
+++ app/src/main/java/com/clayster/xmppiotdemo/MainActivity.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright ⓒ 2016 Florian Schmaus.
+ *
+ * This file is part of XIOT.
+ *
+ * XIOT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XIOT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XIOT.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.clayster.xmppiotdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.asmack.core.AndroidSmack;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AndroidSmack.test();
+    }
+}
